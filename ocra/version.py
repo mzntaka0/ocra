622f49ff3aa06f1c5eb51c11de36bafea86d6ffa
mzntaka0 <takao.mizuno110@gmail.com>
2018-12-13 02:45:08 +0900

    added setup.py

---
+++ ocra/version.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Metadata of package."""
+__version__ = '0.1.0'
+__title__ = 'ocra'
+__description__ = "OCR dataset maker"
+__url__ = ''
+__author__ = 'Takao Mizuno'
+__author_email__ = ''
+__license__ = 'Apache Software License'
